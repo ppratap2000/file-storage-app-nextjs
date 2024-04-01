0e6e5999bf9e875d1c23cca052439d02520a4f74
ppratap <pranav.durapid@outlook.com>
2024-04-01 11:29:24 +0530

    clear all

--- convex/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  providers: [
-    {
-      domain: "https://poetic-salmon-66.clerk.accounts.dev",
-      applicationID: "convex",
-    },
-  ],
-};
--- src/app/dashboard/files/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FileBrowser } from "../_components/file-browser";
-
-export default function FilesPage() {
-  return (
-    <div>
-      <FileBrowser title="Your Files" />
-    </div>
-  );
-}
